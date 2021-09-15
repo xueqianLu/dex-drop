c8b33544ab35490a55af185d21951dccf8681c55
luxq <xueqian1991@gmail.com>
2021-09-15 19:27:05 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hpdex-project/dex-drop
+
+go 1.15
+
+require github.com/ethereum/go-ethereum v1.10.8
---
+++ src/config/config.go
@@ -0,0 +1,8 @@
+package config
+
+import "github.com/ethereum/go-ethereum/common"
+
+var (
+	USDT_Contract = common.HexToAddress("0xe78984541A634C52C760fbF97ca3f8E7d8f04C85")
+	WHPB_Contract = common.HexToAddress("0xBE05Ac1FB417c9EA435b37a9Cecd39Bc70359d31")
+)
